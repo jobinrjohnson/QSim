e3a2b560f138e8c3c8871ca8089eea1e37e53be8
Jobin Johnson <jobinrjohnson@gmail.com>
2019-01-27 13:53:27 +0530

    Add util static library print functionality added
    Signed-off-by: Jobin Johnson <jobinrjohnson@gmail.com>

---
+++ utils/include/util.h
@@ -0,0 +1,13 @@
+/*
+ * util.h
+ *
+ *  Created on: 27 Jan 2019
+ *      Author: jobinrjohnson
+ */
+
+#ifndef UTILS_UTIL_H_
+#define UTILS_UTIL_H_
+
+extern void printdebug(const char*);
+
+#endif /* UTILS_UTIL_H_ */
---
+++ utils/src/util.cpp
@@ -0,0 +1,15 @@
+/*
+ * util.cpp
+ *
+ *  Created on: 27 Jan 2019
+ *      Author: jobinrjohnson
+ */
+
+#include <util.h>
+#include <iostream>
+
+void printdebug(const char* message){
+	std::cout << message;
+	// TODO add debugging options
+}
+
