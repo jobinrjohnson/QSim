4cd20874b320b7354437b002cca512c80b80b46c
Jobin Johnson <jobinrjohnson@gmail.com>
2019-01-27 11:44:10 +0530

    Add initial project files Main Cpp file Cmake files
    Signed-off-by: Jobin Johnson <jobinrjohnson@gmail.com>

---
+++ QSim.cpp
@@ -0,0 +1,17 @@
+/*
+ * QSim.cpp
+ *
+ *  Created on: 27 Jan 2019
+ *      Author: jobinrjohnson
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	cout << "Hey There";
+	return 0;
+
+}
+
