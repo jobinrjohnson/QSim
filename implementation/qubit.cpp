d45ff05874064009ad4e40ef3c36361c5516ea9d
Jobin Johnson <jobinrjohnson@gmail.com>
2019-01-28 20:44:05 +0530

    Use library function for finding absolute value earlier implementation is wrong since we need addition

--- implementation/qubit.cpp
+++ implementation/qubit.cpp
@@ -9,16 +9,16 @@
 
 qubit::qubit() {
 	// initialize the qubit to zero
-	this->state_alpha = std::complex<COMPLEX_TEMPLATE>(1.0,0.0);
-	this->state_beta = std::complex<COMPLEX_TEMPLATE>(0.0,0.0);
+	this->state_alpha = std::complex<COMPLEX_TEMPLATE>(1.0, 0.0);
+	this->state_beta = std::complex<COMPLEX_TEMPLATE>(0.0, 0.0);
 }
 
-qubit::qubit(COMPLEX_TEMPLATE a, COMPLEX_TEMPLATE b){
+qubit::qubit(COMPLEX_TEMPLATE a, COMPLEX_TEMPLATE b) {
 
-	std::complex<COMPLEX_TEMPLATE> state_A(a,0.0);
-	std::complex<COMPLEX_TEMPLATE> state_B(b,0.0);
+	std::complex<COMPLEX_TEMPLATE> state_A(a, 0.0);
+	std::complex<COMPLEX_TEMPLATE> state_B(b, 0.0);
 
-	if(!this->check_normalization(state_A,state_B)){
+	if (!this->check_normalization(state_A, state_B)) {
 		throw "Incorrect Qubit formation";
 		return;
 	}
@@ -29,20 +29,19 @@ qubit::qubit(COMPLEX_TEMPLATE a, COMPLEX_TEMPLATE b){
 
 }
 
+bool qubit::check_normalization(std::complex<COMPLEX_TEMPLATE> state_A,
+		std::complex<COMPLEX_TEMPLATE> state_B) {
 
-bool qubit::check_normalization(std::complex<COMPLEX_TEMPLATE> state_A, std::complex<COMPLEX_TEMPLATE> state_B){
+	double a = abs(state_A);
+	double b = abs(state_B);
 
-	double a = pow(state_A.real(),2) - pow(state_A.imag(),2);
-	double b = pow(state_B.real(),2) - pow(state_B.imag(),2);
-
-	if(( a*a + b*b ) == 1){
+	if ((a * a + b * b) == 1) {
 		return true;
 	}
 
 	return false;
 
 }
-
 
 qubit::~qubit() {
 	// TODO Auto-generated destructor stub
