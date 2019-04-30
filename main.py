63b33b2e0917d8f5f8b45e56a29d5a416285de86
Jobin Johnson <jobinrjohnson@gmail.com>
2019-04-30 17:25:41 +0530

    Switch to flask version better support in heroku

--- main.py
+++ main.py
@@ -1,53 +1,36 @@
-from subprocess import Popen, PIPE
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
-import cgi
+from flask import Flask, jsonify, request, render_template
 import time
+from subprocess import Popen, PIPE
+app = Flask(__name__)
 
 
-class S(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        f=open("html/main.html", "r")
-        self.wfile.write(f.read())
-
-    def do_POST(self):
-
-        form = cgi.FieldStorage(
-            fp=self.rfile,
-            headers=self.headers,
-            environ={'REQUEST_METHOD': 'POST'}
-        )
-
-        code = form.getvalue("code")
-
-        file_name = "temp/" +str(time.time()) + ".txt"
-
-        file1 = open(file_name,"w")
-        file1.write(code) 
-        file1.close() 
-
-        process = Popen(["build/QSim", file_name], stdout=PIPE)
-        (output, err) = process.communicate()
-        exit_code = process.wait()
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(output)
+def serve_homepage():
+    f=open("html/main.html", "r")
+    return f.read()
 
 
-def startServer(port=8090):
-    server_address = ('', port)
-    httpd = HTTPServer(server_address, S)
-    print('Starting server...')
-    httpd.serve_forever()
+def process_post():
+    code = request.form['code']
+
+    file_name = "temp/" +str(time.time()) + ".txt"
+
+    file1 = open(file_name,"w")
+    file1.write(code)
+    file1.close()
+
+    process = Popen(["build/QSim", file_name], stdout=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+
+    return output
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        return process_post()
+    else:
+        return serve_homepage()
 
 
-if __name__ == "__main__":
-    startServer()
+if __name__ == '__main__':
+    app.run()
