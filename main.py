f4a9e82508a05aea868e224d325ff1e2b91d8d58
Jobin Johnson <jobinrjohnson@gmail.com>
2019-05-06 05:19:21 +0530

    redirect http to https

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, make_response
+from flask import Flask, jsonify, request, render_template, make_response, redirect
 import time
 from subprocess import Popen, PIPE
 import os
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder="web", template_folder="web")
 
 
 def serve_homepage():
-    return render_template("main.html", ga_id=os.getenv('GOOGLE_ANALYTICS_ID',""))
+    return render_template("main.html", ga_id=os.getenv('GOOGLE_ANALYTICS_ID', ""))
 
 
 def process_post():
@@ -24,6 +24,7 @@ def process_post():
 
     return output
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -36,6 +37,14 @@ def index():
 def sw_js():
     headers = {'Content-Type': 'application/javascript'}
     return make_response(render_template('sw.js'), 200, headers)
+
+
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 
 if __name__ == '__main__':
